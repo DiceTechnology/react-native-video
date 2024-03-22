9669dae27d043b66d66bd3a4426abaf01cd58018
LIUWEI <liuwei10074180@163.com>
2024-03-22 11:12:46 +0800

    support skip labels

--- android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactTVExoplayerView.java
+++ android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactTVExoplayerView.java
@@ -1741,6 +1741,8 @@ class ReactTVExoplayerView extends FrameLayout implements LifecycleEventListener
                     .setPlayingLiveLabel(translations.getPlayingLiveLabel())
                     .setNowPlayingLabel(translations.getNowPlayingLabel())
                     .setAudioAndSubtitlesLabel(translations.getAudioAndSubtitlesLabel())
+                    .setSkipIntroLabel(translations.getSkipIntroLabel())\
+                    .setSkipCreditsLabel(translations.getSkipCreditsLabel())
                     .build();
 
             exoDorisPlayerView.setLabels(labels);
