41318266d88db47b88c867519ede1f9677cece0d
wei.liu <wei.liu@endeavorstreaming.com>
2024-12-24 10:41:52 +0800

    bump doris-android

--- android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactTVExoplayerViewManager.java
+++ android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactTVExoplayerViewManager.java
@@ -82,7 +82,7 @@ public class ReactTVExoplayerViewManager extends ViewGroupManager<ReactTVExoplay
     private static final String PROP_SRC_DVR_SEEK_FORWARD_INTERVAL = "dvrSeekForwardInterval";
     private static final String PROP_SRC_PLUGINS = "plugins";
     private static final String PROP_SRC_LIVE = "live";
-    private static final String PROP_SRC_AUDIO_ONLY = "audioOnly";
+    private static final String PROP_SRC_AUDIO_ONLY = "isAudioOnly";
 
     // Metadata properties
     private static final String PROP_METADATA = "metadata";
