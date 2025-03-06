beb99c059e9bd40e5134a99ddab720e04432f67d
Nick Wang <nick.wang@endeavorstreaming.com>
2025-03-06 15:23:49 +0800

    DORIS-2749 Multiview  TVOS in progress

--- ios/JSProps/Buttons.swift
+++ ios/JSProps/Buttons.swift
@@ -17,7 +17,7 @@ struct Buttons: SuperCodable {
     let stats: Bool
     let favourite: Bool
     let info: Bool?
-    let multiView: Bool?
+    let multiview: Bool?
     //ios only
     let fullscreen: Bool?
     let zoom: Bool?//ios
--- types/buttons.ts
+++ types/buttons.ts
@@ -4,5 +4,5 @@ export interface IVideoPlayerButtons {
   epg?: boolean;
   stats?: boolean;
   annotations?: boolean;
-  multiView?: boolean;
+  multiview?: boolean;
 }
