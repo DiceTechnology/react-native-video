d14358f4b313db94cec24b69ffe0781b3b981fd3
wei.liu <wei.liu@endeavorstreaming.com>
2025-04-30 14:19:23 +0800

    fix: upgrade multiview guide message and bump doris version

--- android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactTvMultipleExoplayerView.kt
+++ android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactTvMultipleExoplayerView.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.view.Gravity
 import android.view.KeyEvent
 import android.view.View
-import android.view.View.OnFocusChangeListener
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import androidx.core.view.children
