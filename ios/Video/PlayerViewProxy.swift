e36dc33bdc2df395a39757346720f83847a497a7
Yaroslav Lvov <yaroslav.lvov@gmail.com>
2025-09-16 15:32:13 +0300

    fix: build issue

--- ios/Video/PlayerViewProxy.swift
+++ ios/Video/PlayerViewProxy.swift
@@ -234,7 +234,7 @@ class PlayerViewProxy {
                 drm: rndvJSDrm,
                 progressUpdateInterval: sourceValue.progressUpdateInterval ?? 6,
                 type: sourceValue.type,
-                title: sourceValue.nowPlaying?.title ?? jsProps.metadata.value?.title ?? sourceValue.title "",
+                title: sourceValue.nowPlaying?.title ?? jsProps.metadata.value?.title ?? sourceValue.title ?? "",
                 description: jsProps.metadata.value?.description, //tvos new
                 live: sourceValue.live,
                 partialVideoInformation: jsPartialVideoInformation,
