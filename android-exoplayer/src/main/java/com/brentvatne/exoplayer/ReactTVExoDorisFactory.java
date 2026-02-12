218e3c90e91d66ee1bbba1692cbb7c872b8c9a4b
Arnold Szabó <szaboaarnold@gmail.com>
2026-02-12 10:38:32 +0100

    fix: update localization service with off and und labels (#491)
    * fix: update localization service with off and und labels
    
    * chore: bump doris to stable version

--- android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactTVExoDorisFactory.java
+++ android-exoplayer/src/main/java/com/brentvatne/exoplayer/ReactTVExoDorisFactory.java
@@ -12,31 +12,10 @@ import com.diceplatform.doris.ExoDorisBuilder;
 import com.diceplatform.doris.common.ad.ui.AdChoicesClickViewRenderer;
 import com.diceplatform.doris.entity.AdType;
 import com.diceplatform.doris.entity.SubtitlesPolicy;
+import com.diceplatform.doris.service.LocalizationService;
+import com.diceplatform.doris.service.ServiceContainer;
 
 public final class ReactTVExoDorisFactory {
-
-    public ExoDoris createPlayer(
-            @NonNull Context context,
-            AdType adType,
-            int loadBufferMs,
-            long forwardIncrementMs,
-            long rewindIncrementMs,
-            @Nullable AdViewProvider adViewProvider,
-            @Nullable AdChoicesClickViewRenderer adChoicesClickViewRenderer,
-            SubtitlesPolicy subtitlesPolicy) {
-        return createPlayer(
-                context,
-                adType,
-                true,
-                null,
-                loadBufferMs,
-                forwardIncrementMs,
-                rewindIncrementMs,
-                null,
-                adViewProvider,
-                adChoicesClickViewRenderer,
-                subtitlesPolicy);
-    }
 
     public ExoDoris createPlayer(
             @NonNull Context context,
@@ -49,7 +28,18 @@ public final class ReactTVExoDorisFactory {
             @Nullable Parameters.Builder parametersBuilder,
             @Nullable AdViewProvider adViewProvider,
             @Nullable AdChoicesClickViewRenderer adChoicesClickViewRenderer,
-            @Nullable SubtitlesPolicy subtitlesPolicy) {
+            @Nullable SubtitlesPolicy subtitlesPolicy,
+            @NonNull LocalizationService localizationService) {
+
+        ReactTVExoDorisExtensionFactory playerExtensionProvider =
+                new ReactTVExoDorisExtensionFactory(
+                        adViewProvider,
+                        adChoicesClickViewRenderer
+                );
+
+        ServiceContainer serviceContainer = new ServiceContainer.Builder()
+                .localizationService(localizationService)
+                .build();
 
         return new ExoDorisBuilder(context)
                 .setEnableManifestScte35(adType == AdType.IMA_CSAI_LIVE)
@@ -60,7 +50,8 @@ public final class ReactTVExoDorisFactory {
                 .setRewindIncrementMs(rewindIncrementMs)
                 .setParamsBuilder(parametersBuilder)
                 .setSubtitlesPolicy(subtitlesPolicy)
-                .setPlayerExtensionProvider(new ReactTVExoDorisExtensionFactory(adViewProvider, adChoicesClickViewRenderer))
+                .setPlayerExtensionProvider(playerExtensionProvider)
+                .setServiceContainer(serviceContainer)
                 .build();
     }
 }
